76e3361ab34296290d2add2e38340cb93ec257b1
olinelson <olivernelson@icloud.com>
2019-05-23 18:16:28 -0400

    found way better react carousel

--- src/Video.js
+++ src/Video.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Slider from "react-slick";
+
 
 import pic1 from "./resources/pic1.jpg";
 import pic2 from "./resources/pic2.jpg";
@@ -9,52 +9,43 @@ import { Embed, Container, Icon } from "semantic-ui-react";
 
 // this sample arrow next stuff is to overide the built in styling of the next buttons.
 // there are overides for the backgorund color in app.css
+import InfiniteCarousel from "react-leaf-carousel";
 
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "transparent" }}
-      onClick={onClick}
-    />
-  );
-}
-
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "transparent" }}
-      onClick={onClick}
-    >
-
-    </div>
-  );
-}
-
-export default class SimpleSlider extends Component {
+export default class Video extends Component {
   render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      swipeToSlide: true,
-      nextArrow: <SampleNextArrow />,
-      prevArrow: <SamplePrevArrow />
-    };
+  
     return (
-      <Container>
-        <h1>Video</h1>
-        <Slider {...settings}>
+        <InfiniteCarousel
+          breakpoints={[
+            {
+              breakpoint: 500,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]}
+          dots={true}
+          showSides={true}
+          sidesOpacity={0.5}
+          sideSize={0.1}
+          slidesToScroll={1}
+          slidesToShow={1}
+          scrollOnDevice={true}
+          swipe={true}
+        >
           <Embed id="NnFzxJgZjiI" placeholder={pic1} source="youtube" />
           <Embed id="4BPnDuYu8Ts" placeholder={pic2} source="youtube" />
           <Embed id="uaN1BwKqg1Q" placeholder={pic3} source="youtube" />
-        </Slider>
-      </Container>
+        </InfiniteCarousel>
+
     );
   }
 }
